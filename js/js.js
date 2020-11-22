b409ca99f07bb8c1efc95f24bb817eb3d1e13803
unknown <sidicleiluiz@hotmail.com>
2020-11-21 21:21:45 -0300

    Adicionando colisao

--- js/js.js
+++ js/js.js
@@ -42,6 +42,8 @@ function start() { // Inicio da função start()
 		moveinimigo1();
 		moveinimigo2();
 		moveamigo();
+		colisao();
+
 
 	} // Fim da função loop()
 
@@ -155,6 +157,15 @@ function start() { // Inicio da função start()
 			}
 		} // Fecha executaDisparo()
 	} // Fecha disparo()
+
+	function colisao() {
+		var colisao1 = ($("#jogador").collision($("#inimigo1")));
+		// jogador com o inimigo1
+	
+		console.log(colisao1);
+	
+	} //Fim da função colisao()
+	
 
 
 
